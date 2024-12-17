67f4b154fc45aafc7cefbed69191f01608e2ef3e
meetpatel35 <patelmeet0625@gmail.com>
2024-12-17 21:14:30 +0530

    add files and data

---
+++ lib/currency_converter.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverter extends StatelessWidget {
+  const CurrencyConverter({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromARGB(255, 255, 0, 0),
+        width: 2.0,
+        style: BorderStyle.none,
+      ),
+      borderRadius: BorderRadius.circular(60),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "0",
+              style:TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.cyanAccent),
+            ),
+            TextField(
+              keyboardType:const TextInputType.numberWithOptions(decimal: true),
+              style:const TextStyle(color: Color.fromARGB(255, 0, 124, 37)),
+              decoration: InputDecoration(
+                focusedBorder: border,
+                enabledBorder: border,
+                hintText: "Please enter the amount",
+                hintStyle:const TextStyle(color: Color.fromARGB(179, 0, 0, 0)),
+                prefixIcon: const Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Color.fromARGB(179, 0, 0, 0),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
